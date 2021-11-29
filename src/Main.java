8231b39b61272cef0995f135e7e9ecb4f2076668
Dunned <eduardo.jauregui1@unmsm.edu.pe>
2021-11-28 20:05:31 -0500

    Primer Commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+       Persona p=new Persona("Eduardo",15);
+        System.out.print("Nombre: "+p.getNombre()+"Sueldo: "+p.getSueldo());
+    }
+}
